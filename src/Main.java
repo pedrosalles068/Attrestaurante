a371d1273f46c9fe5130f95fde657b50616485b9
pedro <pedro.salles@alunos.unis.edu.br>
2024-06-18 17:10:14 -0300

    novo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Cliente c1 = new Cliente("cliente1@gmail.com","João","35988996428");
+        Bebida bebida1 = new Bebida("Coca Cola",15,"servido na torre","2L","Refrigetante");
+        Comida comida1 = new Comida("Pizza",80,"pizza de 12 pedaços","especiais");
+        Pedido p1 = new Pedido(1,c1,"preparando");
+        p1.adicionaItem(comida1);
+        p1.adicionaItem(bebida1);
+
+       p1.imprimirPedido();
+
+
     }
 }
---
+++ src/Pedido.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+    private int numero;
+    private Cliente cliente;
+    private List<IProduto> itens;
+    private String andamento;
+
+    public Pedido(int numero, Cliente cliente, String andamento) {
+        this.numero = numero;
+        this.cliente = cliente;
+        this.itens = new ArrayList<>();
+        this.andamento = andamento;
+    }
+
+    public void adicionaItem(IProduto produto) {
+        itens.add(produto);
+    }
+
+    public void removerItem(IProduto produto) {
+        itens.remove(produto);
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (IProduto item : itens) {
+            total += item.getPreco();
+        }
+        return total;
+    }
+    public void imprimirPedido() {
+        System.out.println("Pedido Número: " + numero);
+        System.out.println("Cliente: " + cliente.getNome() +"\nEmail: " + cliente.getEmail() + "\nTelefone: " + cliente.getTelefone() + "");
+        System.out.println("Andamento: " + andamento);
+        System.out.println("Itens do Pedido:");
+        for (IProduto item : itens) {
+            System.out.println(" - " + item.getDescricao() + ": R$" + item.getPreco());
+        }
+        System.out.println("Total do pedido: R$" + calcularTotal());
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public List<IProduto> getItens() {
+        return itens;
+    }
+
+    public String getAndamento() {
+        return andamento;
+    }
+}
